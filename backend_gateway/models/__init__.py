8f4c37ef435b83363c54668e140e7aba82af60e3
Daniel Kim <starfleetoptimus@gmail.com>
2025-05-25 00:35:52 -0400

    Update header to show 'PrepSense' consistently and fix button visibility

---
+++ backend_gateway/models/__init__.py
@@ -0,0 +1,9 @@
+# This file makes the models directory a Python package
+# Import models here to make them available when importing from backend_gateway.models
+from .user import UserCreate, UserInDB, UserResponse
+
+__all__ = [
+    'UserCreate',
+    'UserInDB',
+    'UserResponse',
+]
