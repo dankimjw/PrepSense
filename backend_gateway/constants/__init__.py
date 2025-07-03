39b4802c24951547b62b3128f6bac3b5d2323919
PrepSense Team <team@prepsense.com>
2025-07-03 01:35:20 -0400

    feat: export unit constants from backend module

--- backend_gateway/constants/__init__.py
+++ backend_gateway/constants/__init__.py
@@ -1,1 +1,23 @@
 # Constants module for PrepSense backend
+
+from .units import (
+    Unit,
+    UnitCategory,
+    UNIT_INFO,
+    UNIT_CONVERSIONS,
+    UNIT_VARIATIONS,
+    normalize_unit,
+    get_unit_category,
+    convert_quantity
+)
+
+__all__ = [
+    'Unit',
+    'UnitCategory',
+    'UNIT_INFO',
+    'UNIT_CONVERSIONS',
+    'UNIT_VARIATIONS',
+    'normalize_unit',
+    'get_unit_category',
+    'convert_quantity'
+]
