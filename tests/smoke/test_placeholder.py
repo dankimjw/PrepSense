e161fa8aa4b9a580f60b0a2512d68f8c5964d712
Daniel Kim <dankimjw@gmail.com>
2025-08-04 11:48:39 -0400

    Remove tracked files now ignored

--- tests/smoke/test_placeholder.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-# GIVEN a brand new project scaffold
-# WHEN the test pipeline runs initially
-# THEN this placeholder test should fail until implementation is complete
-
-def test_placeholder():
-    pytest.fail("Not yet implemented")
